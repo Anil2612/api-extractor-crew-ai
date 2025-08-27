43a154c3464f528c262d038f574c072111e20a96
Anil S Nair <anilsnair261296@gmail.com>
2025-08-28 00:55:10 +0530

    feat: add posthog

--- api-extractor-fe/src/main.jsx
+++ api-extractor-fe/src/main.jsx
@@ -4,6 +4,14 @@ import "./index.scss";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import posthog from 'posthog-js'
+
+posthog.init('phc_jisKIOvB2pvEp2n6CKLnANwILZ6KzpGGLdF9OT96IGD',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
+    }
+)
 
 const queryClient = new QueryClient();
 
